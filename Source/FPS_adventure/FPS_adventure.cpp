6fa74d89e4c32c5d55aac85800af1ae62d1e29ef
skyhammer1995 <39307121+skyhammer1995@users.noreply.github.com>
2025-06-09 02:16:07 -0400

    5.6 all building

---
+++ Source/FPS_adventure/FPS_adventure.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPS_adventure.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPS_adventure, "FPS_adventure" );
